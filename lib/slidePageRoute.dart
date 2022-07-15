d4238d556814839ac1ac245e8b7c70e010b7a630
zerodeaths000 <wrongprogramcode@gmail.com>
2022-07-15 03:06:53 +0200

    Added progress indicator, and sliding page route

---
+++ lib/slidePageRoute.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SlidePageRoute extends PageRouteBuilder {
+  final Widget child;
+
+  SlidePageRoute({required this.child})
+      : super(
+            pageBuilder: (context, anim, secondAnim) => child,
+            transitionDuration: Duration(milliseconds: 1000));
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+          Animation<double> secondAnim, Widget child) =>
+      SlideTransition(
+        position: Tween<Offset>(begin: Offset(-1, 0), end: Offset.zero)
+            .animate(animation),
+        child: child,
+      );
+}
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:unitix/slidePageRoute.dart';
+
 import 'pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:unitix/splash.dart';
@@ -15,6 +18,10 @@ class VideoPlayerScreen extends StatefulWidget {
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   final asset = "assets/leaf_splash.mp4";
+  late Timer _valueTimer;
+  late Duration _pageTimeDuration = Duration(microseconds: 1);
+  int _pageTime = 0;
+  double valueIndicator = 0.0;
 
   @override
   void initState() {
@@ -26,18 +33,45 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void navigate() async {
-    await Future.delayed(const Duration(seconds: 5));
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => MyHomePage()));
+    _valueTimer = Timer.periodic(_pageTimeDuration, (timer) {
+      setState(() {
+        if (_pageTime == 10000) {
+          timer.cancel();
+          Navigator.of(context).push(SlidePageRoute(child: MyHomePage()));
+        } else {
+          _pageTime++;
+          debugPrint(_pageTime.toString());
+          valueIndicator = _pageTime / 10000;
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Splash(
-        controller: VideoPlayerController.asset(asset),
-      )),
+    return Stack(
+      children: [
+        Splash(
+          controller: VideoPlayerController.asset(asset),
+        ),
+        Positioned(
+          bottom: 100,
+          child: Container(
+            alignment: Alignment.center,
+            width: 300,
+            height: 30,
+            child: LiquidLinearProgressIndicator(
+              backgroundColor: Colors.black,
+              valueColor: AlwaysStoppedAnimation(Colors.greenAccent),
+              borderColor: Colors.white,
+              borderWidth: 1,
+              direction: Axis.horizontal,
+              value: valueIndicator,
+            ),
+          ),
+        )
+      ],
+      alignment: Alignment.center,
     );
   }
 }
